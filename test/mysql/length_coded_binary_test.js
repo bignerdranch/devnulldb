35ba5b202a5f9eaae578d190cbe876028bb73302
Andy Lindeman <andy@highgroove.com>
2012-02-23 20:33:32 -0500

    LengthCodedBinary can parse numbers

--- test/mysql/length_coded_binary_test.js
+++ test/mysql/length_coded_binary_test.js
@@ -43,4 +43,48 @@ describe("LengthCodedBinary", function() {
       assert.equal("\xFE\x00\x00\x00\x00\xFE\xFF\xFF\xFF", lcb.toBytes());
     });
   });
+
+  describe("deserialization", function() {
+    it("deserializes correctly when value = null", function() {
+      var lcb = LengthCodedBinary.parse("\xFB");
+
+      assert.equal(null, lcb.getValue());
+    });
+
+    it("serializes correctly when value is between 0-250", function() {
+      var lcb = LengthCodedBinary.parse("\x00");
+      assert.equal(0, lcb.getValue());
+
+      lcb = LengthCodedBinary.parse("\x64");
+      assert.equal(100, lcb.getValue());
+
+      lcb = LengthCodedBinary.parse("\xFA");
+      assert.equal(250, lcb.getValue());
+    });
+
+    it("serializes correctly when value is represented as 2 bytes", function() {
+      var lcb = LengthCodedBinary.parse("\xFC\xFE\xFF");
+
+      assert.equal(65534, lcb.getValue());
+    });
+
+    it("serializes correctly when value is represented as 3 bytes", function() {
+      var lcb = LengthCodedBinary.parse("\xFD\xFE\xFF\xFF");
+
+      // 253 followed by a 24-bit word
+      assert.equal(16777214, lcb.getValue());
+    });
+
+    it("serializes correctly when value is represented as 8 bytes", function() {
+      var lcb = LengthCodedBinary.parse("\xFE\x00\x00\x00\x00\xFF\xFE\xFD\x7C");
+
+      assert.equal(2097020671, lcb.getValue());
+    });
+
+    it("does not allow overruns into negative terrority due to the javascript using 32-bit signed integers for bitwise operations", function() {
+      var lcb = LengthCodedBinary.parse("\xFE\x00\x00\x00\x00\xFF\xFE\xFD\xFC");
+
+      assert.equal(2097020671, lcb.getValue());
+    });
+  });
 });
